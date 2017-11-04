474bba35a6d65b5aedbd34b4d9cf09d6a57239d6
stud40 <stud40@icsmaster01.cluster.net>
2017-11-04 19:03:31 +0100

    added 512 3 layer

--- bjob.sh
+++ bjob.sh
@@ -10,10 +10,9 @@
 module load python/3.5.0
 module load cudnn/8.0
 
-
 save_dir="save_"$1
-rnn_size=512
-num_layers=3
+rnn_size=256
+num_layers=2
 learning_rate=0.001
 num_epochs=100
 data_dir='data/gio'
@@ -21,8 +20,9 @@ batch_size=50
 seq_length=100
 output_keep_prob=1.0
 input_keep_prob=1.0
-grad_clip=5.
+grad_clip=5.0
 decay_rate=0.97
 
-srun python3 train.py --save_dir=$save_dir --rnn_size=$rnn_size --learning_rate=$learning_rate --num_layers=$num_layers --output_keep_prob=$output_keep_prob --input_keep_prob=$input_keep_prob
- --grad_clip=$grad_clip --decay_rate=$decay_rate --num_epochs=$num_epochs --batch_size=$batch_size --seq_length=$seq_length --data_dir=$data_dir
+srun python3 train.py --save_dir=$save_dir --rnn_size=$rnn_size --learning_rate=$learning_rate --num_layers=$num_layers \
+--output_keep_prob=$output_keep_prob --input_keep_prob=$input_keep_prob --grad_clip=$grad_clip --decay_rate=$decay_rate \
+--num_epochs=$num_epochs --batch_size=$batch_size --seq_length=$seq_length --data_dir=$data_dir
